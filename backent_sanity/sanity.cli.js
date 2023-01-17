25e030fb3f800d65f4c2defad7402ad89b280b29
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-17 21:03:22 +0545

    initialized

---
+++ backent_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'culuboo8',
+    dataset: 'production'
+  }
+})
---
+++ backent_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import about from './abouts'
+import contact from './contact'
+import brand from './brands'
+import experience from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  testimonials,
+  about,
+  contact,
+  brand,
+  experience,
+  skills,
+  workExperience,
+  works,
+]
