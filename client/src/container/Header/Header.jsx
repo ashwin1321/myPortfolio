63da96030be2546aac9f514fe3f40a92b487afa3
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-19 15:42:21 +0545

    header section updated

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.scss";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
 
 const scaleVariants = {
   whileInView: {
@@ -75,4 +76,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default AppWrap(Header, "home");
