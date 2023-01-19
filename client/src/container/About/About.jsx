d9c836181513bbfe0cbe1153c1f13da45ac59675
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-19 15:07:50 +0545

    about section updated

--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import { urlFor, client } from "../../client";
 import "./About.scss";
 
+// const abouts = [
+//   {
+//     title: "Backend Development",
+//     description: "abcd",
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: "Web3",
+//     description: "abcd",
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: "Blockchain",
+//     description: "abcd",
+//     imgUrl: images.about03,
+//   },
+// ];
+
 const About = () => {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text" style={{ margin: "2rem" }}>
+        I Know That
+        <span> Good Design </span>
+        <br />
+        means
+        <span> Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt="img" />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
