72bc2bf47ed44be3028d627c10bac87f5009f62d
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-19 15:07:03 +0545

    connected to sanity

---
+++ client/src/client.js
@@ -0,0 +1,18 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+// require("dotenv").config();
+
+export const client = sanityClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2022-02-01",
+  useCdn: true,
+  token: import.meta.env.VITE_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => {
+  return builder.image(source);
+};
