4afb0046f1538efa799a5d095e65185f5f6ce6e2
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-19 15:41:57 +0545

    about section updated

--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
 import { urlFor, client } from "../../client";
+import { AppWrap } from "../../wrapper";
 import "./About.scss";
 
 // const abouts = [
@@ -64,4 +65,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "about");
