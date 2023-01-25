7c0ad5c7e2d159a2e471816782e953112e6822f2
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-25 20:00:51 +0545

    skills section updated

--- client/src/container/Skills/Skills.jsx
+++ client/src/container/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Tooltip as ReactTooltip } from "react-tooltip";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Skills.scss";
 const Skills = () => {
@@ -83,4 +83,8 @@ const Skills = () => {
   );
 };
 
-export default AppWrap(Skills);
+export default AppWrap(
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__whitebg"
+);
