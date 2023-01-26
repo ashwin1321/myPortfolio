d00002d6ec8956d5ec5a9e0cdaa1a7cef1ccd440
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-26 15:32:44 +0545

    updated

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -34,8 +34,10 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
+import emaill from "../assets/email.jpg";
 
 export default {
+  emaill,
   email,
   mobile,
   api,
